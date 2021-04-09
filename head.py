f76ae5db26d280c14f565fa072345959447a70fe
Wesley <shiwenlei2012@gmail.com>
2021-04-09 20:32:04 +0800

    pg support

--- head.py
+++ head.py
@@ -1,7 +1,12 @@
 import ray
 import asyncio
 import torch
-import time
+# Import placement group APIs.
+from ray.util.placement_group import (
+    placement_group,
+    placement_group_table,
+    remove_placement_group
+)
 
 
 @ray.remote(num_cpus=5, num_gpus=1)
@@ -13,16 +18,26 @@ class AsyncActor():
         print("start")
         await asyncio.sleep(1) # Network, I/O task here
         value = torch.randn(8)
-        return value
         print("end")
+        return {
+            "value": value,
+            "": 0
+            }
 
-"""
-def f():
-    time.sleep(0.01)
-    return ray._private.services.get_node_ip_address(), ray._private.service.get_node_port
-"""
+bundle1 = {"GPU": 1}
+bundle2 = {"CPU": 5}
 
-ray.init(address="auto",  _redis_password='123')
+pg = placement_group([bundle1, bundle2], strategy="STRICT_PACK")
+# Wait until placement group is created.
+ray.get(pg.ready())
+
+# You can also use ray.wait.
+ready, unready = ray.wait([pg.ready()], timeout=0)
+
+# You can look at placement group states using this API.
+print(placement_group_table(pg))
+
+ray.init(address="auto", _redis_password='123')
 actor = AsyncActor.options(max_concurrency=2).remote()
 
 # Get a list of the IP addresses of the nodes that have joined the cluster.
