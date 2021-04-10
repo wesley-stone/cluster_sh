3896e80cc80ddbcd3b04961298ab3405f07930bc
Wesley <shiwenlei2012@gmail.com>
2021-04-10 10:46:35 +0800

    toy case of actor critic

--- head.py
+++ head.py
@@ -1,6 +1,9 @@
+from time import sleep
 import ray
 import asyncio
 import torch
+import random
+import pprint
 # Import placement group APIs.
 from ray.util.placement_group import (
     placement_group,
@@ -8,40 +11,58 @@ from ray.util.placement_group import (
     remove_placement_group
 )
 
+bundle1 = {"GPU": 0.25, "CPU": 1}
 
-@ray.remote(num_cpus=5, num_gpus=1)
+@ray.remote(num_cpus=bundle1['CPU'], num_gpus=bundle1['GPU'])
 class AsyncActor():
-    def __init__(self):
-        pass
+    def __init__(self, actor_id):
+        self.id = actor_id
 
-    async def rollout(self):
-        print("start")
-        await asyncio.sleep(1) # Network, I/O task here
-        value = torch.randn(8)
-        print("end")
+    async def rollout(self, parameters, batch_size=4):
+        sleep_sec = random.randint(0, 10)
+        await asyncio.sleep(sleep_sec) # Network, I/O task here
+        value = torch.randn(batch_size, 1)
+        input = torch.randn(batch_size, 2)
         return {
+            "input": input,
             "value": value,
-            "": 0
-            }
+            "actor_id": self.id,
+        }
 
-bundle1 = {"GPU": 1}
-bundle2 = {"CPU": 5}
 
-pg = placement_group([bundle1, bundle2], strategy="STRICT_PACK")
+tot_cpus = 15
+tot_gpus = 3
+ray.init(address="auto", _redis_password='123', num_gpus=tot_gpus, num_cpus=tot_cpus)
+
+pg_cnt = min(int(tot_gpus/bundle1['GPU']), int(tot_cpus/bundle1['CPU']))
+
+pgs = [placement_group(bundle1, strategy="STRICT_PACK") for _ in range(pg_cnt)]
 # Wait until placement group is created.
-ray.get(pg.ready())
-
-# You can also use ray.wait.
-ready, unready = ray.wait([pg.ready()], timeout=0)
+ray.get([pg.ready() for pg in pgs])
+for pg in pgs:
+    print(placement_group_table(pg))
 
 # You can look at placement group states using this API.
 print(placement_group_table(pg))
 
-ray.init(address="auto", _redis_password='123')
-actor = AsyncActor.options(max_concurrency=2).remote()
+actors = [AsyncActor.options(placement_group=pg).remote() for pg in pgs]
 
 # Get a list of the IP addresses of the nodes that have joined the cluster.
-for i in range(10):
-    results = ray.get([actor.rollout.remote() for _ in range(5)])
-    for j in range(5):
-        print(results[j])
+epoch = 5
+batch_size = 4
+bunch_size = 2
+parameters = {"conv1x1": 1}
+data_ids = [actor.rollout.remote(parameters, batch_size) for actor in actors]
+for i in range(epoch):
+    pprint(f"collection start - {epoch}")
+    done_ids = [ray.wait(data_ids) for _ in range(bunch_size)]
+    batch_data = [ray.get(done_id) for done_id in done_ids]
+    pprint(f"collection end - {epoch}")
+    for bd in batch_data:
+        print(bd['input'])
+    pprint(f"training start - {epoch}")
+    # pushing to the replay buffer
+    # training...
+    sleep(3)
+    pprint(f"training end - {epoch}")
+    data_ids.extend([actors[bd['actor_id']].rollout.remote(parameters, batch_size) for bd in batch_data])
